eeda0a7a44e049562010b8064ff0cead7099c3e0
wilber <parionahuaraca@gmail.com>
2021-07-06 19:50:44 -0500

    cambios

--- index.php
+++ index.php
@@ -190,8 +190,8 @@
     <div class="section_middle_justify w_80 w_85_desktop">
       <div class="w_100 w_40_desktop section_middle_justify"  >
         <div class="w_100 w_50_desktop whiteColor">
-          <p>Av. Del Ejército 1180 Of. 304,</p>
-          <p>Magdalena del Mar 15076</p>
+          <p>Av. Pasaje Mártir Olaya 129 Int. 1905,</p>
+          <p>Miraflores</p>
           <p>+ (51) 998 315 039</p>
         </div>
         <a href="mailto:contacto@tendenzeperu.com" class="buble">Contacto</a>
