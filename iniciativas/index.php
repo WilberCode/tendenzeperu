61e2b2d63132b2f94b8547c8f464c2f64bae7353
wilber <parionahuaraca@gmail.com>
2023-05-31 23:39:10 -0500

    cambios

--- iniciativas/index.php
+++ iniciativas/index.php
@@ -50,23 +50,32 @@
   <meta http-equiv="pragma" content="no-cache" />
 
 <!-- Google Tag Manager -->
-<script>(function (w, d, s, l, i) {
-  w[l] = w[l] || []; w[l].push({
-    'gtm.start':
-      new Date().getTime(), event: 'gtm.js'
-  }); var f = d.getElementsByTagName(s)[0],
-    j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-      'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-  })(window, document, 'script', 'dataLayer', 'GTM-PZPR3GG');</script>
+<script> 
+  setTimeout(function(){ 
+    (function (w, d, s, l, i) {
+    w[l] = w[l] || []; w[l].push({
+      'gtm.start':
+        new Date().getTime(), event: 'gtm.js'
+    }); var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-PZPR3GG');
+  },3000);  
+  </script>
 <!-- End Google Tag Manager -->
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156909426-1"></script>
+ 
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LKGQELEQS2"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag() { dataLayer.push(arguments); }
-  gtag('js', new Date());
-  gtag('config', 'UA-156909426-1');   
+     setTimeout(function(){ 
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-LKGQELEQS2');
+     },3000);  
 </script>
 
  
--- videos/index.php
+++ videos/index.php
@@ -39,6 +39,34 @@
   <meta http-equiv="expires" content="-1" />
   <meta http-equiv="expires" content="Sun May 23 2021 02:00:50 GMT-0500" />
   <meta http-equiv="pragma" content="no-cache" />
+<!-- Google Tag Manager -->
+<script> 
+  setTimeout(function(){ 
+    (function (w, d, s, l, i) {
+    w[l] = w[l] || []; w[l].push({
+      'gtm.start':
+        new Date().getTime(), event: 'gtm.js'
+    }); var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-PZPR3GG');
+  },3000);  
+  </script>
+<!-- End Google Tag Manager -->
+
+ 
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LKGQELEQS2"></script>
+<script>
+     setTimeout(function(){ 
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-LKGQELEQS2');
+     },3000);  
+</script>
 
 </head>
 <body>
