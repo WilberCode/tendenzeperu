81500f94942bee8a3338097043095f7e5d32dbe0
wilber <parionahuaraca@gmail.com>
2023-05-31 23:33:43 -0500

    cambios

--- index.php
+++ index.php
@@ -61,6 +61,7 @@
 
 <!-- Google Tag Manager -->
 <script> 
+  setTimeout(function(){ 
     (function (w, d, s, l, i) {
     w[l] = w[l] || []; w[l].push({
       'gtm.start':
@@ -69,19 +70,24 @@
       j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
         'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
     })(window, document, 'script', 'dataLayer', 'GTM-PZPR3GG');
- 
+  },3000);  
   </script>
 <!-- End Google Tag Manager -->
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156909426-1"></script>
-<script> 
+ 
 
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
-    gtag('config', 'UA-156909426-1');    
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LKGQELEQS2"></script>
+<script>
+     setTimeout(function(){ 
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-LKGQELEQS2');
+     },3000);  
 </script>
+
 
  <script type="application/ld+json">
   {
