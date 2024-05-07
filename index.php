f0314673a798e0196e5067888f61aa5437ccce85
wilber <parionahuaraca@gmail.com>
2024-05-06 19:30:59 -0500

    cambios

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
 
 <!-- Google Tag Manager -->
 <script> 
-/*   setTimeout(function(){ 
+  setTimeout(function(){ 
     (function (w, d, s, l, i) {
     w[l] = w[l] || []; w[l].push({
       'gtm.start':
@@ -70,22 +70,22 @@
       j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
         'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
     })(window, document, 'script', 'dataLayer', 'GTM-PZPR3GG');
-  },3000);   */
+  },3000);  
   </script>
 <!-- End Google Tag Manager -->
 
  
 
 <!-- Google tag (gtag.js) -->
-<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-LKGQELEQS2"></script> -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LKGQELEQS2"></script>
 <script>
-     /* setTimeout(function(){ 
+     setTimeout(function(){ 
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
         gtag('config', 'G-LKGQELEQS2');
-     },3000);   */
+     },3000);  
 </script>
 
 
