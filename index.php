ede0cfc790e738099e4881b8db34e8737f9e1a27
wilber <parionahuaraca@gmail.com>
2024-10-14 20:08:21 -0500

    cambios

--- index.php
+++ index.php
@@ -315,7 +315,7 @@
                   <?php  
                   foreach($clients['clientes'] as $client){   ?>
                     <figure class="<?=$client['tipo'] ?>">  
-                      <img  src="/<?=$client['img'] ?>" alt="">
+                        <img  src="/<?=$client['img'] ?>" alt="<?=$client['title'] ?>">
                    </figure>  
                  <?php  }  ?>
              
